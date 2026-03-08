46b6d953d3b42c51fd8bea6b6cbd2de09de67e21
bhanu-3333 <bhanuu339@gmail.com>
2026-03-08 13:57:56 +0530

    feat: Add GrowthSection component featuring scroll-animated service tags and establish core application and home page styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './pages/Home/Home';
+import GrowthSection from './components/GrowthSection/GrowthSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <GrowthSection />
     </div>
   );
 }
