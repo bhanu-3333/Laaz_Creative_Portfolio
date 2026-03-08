f61ccd69b3470c8ac40b6df56842b8e0ebd7dc58
bhanu-3333 <bhanuu339@gmail.com>
2026-03-08 14:27:25 +0530

    feat: Add Approach Section with an auto-looping card stack animation and descriptive content.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/Home/Home';
 import GrowthSection from './components/GrowthSection/GrowthSection';
+import ApproachSection from './components/ApproachSection/ApproachSection';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Home />
       <GrowthSection />
+      <ApproachSection />
     </div>
   );
 }
