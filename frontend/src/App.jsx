25d0130ac8a3721f0306039d0aa4a9f8be5c4026
bhanu-3333 <bhanuu339@gmail.com>
2026-03-17 13:57:08 +0530

    feat: Add initial portfolio frontend with Home and Enquire pages, and core UI components including animations, sections, and a footer.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import PortfolioPage from './pages/Portfolio/PortfolioPage';
+import EnquirePage from './pages/Enquire/EnquirePage';
 import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/enquire" element={<EnquirePage />} />
         </Routes>
       </div>
     </BrowserRouter>
